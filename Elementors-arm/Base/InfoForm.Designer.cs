5b12c310c3a2e13abbb19ae46c95ec7d55d96207
Exoticness <roof1rst@gmail.com>
2015-02-02 23:07:55 +0300

    Added Info Form

---
+++ Elementors-arm/Base/InfoForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Elementors_arm
+{
+    partial class InfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ribbon = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.ribbonStatusBar = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+            this.gridControl = new DevExpress.XtraGrid.GridControl();
+            this.gridView = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ribbon
+            // 
+            this.ribbon.ExpandCollapseItem.Id = 0;
+            this.ribbon.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbon.ExpandCollapseItem});
+            this.ribbon.Location = new System.Drawing.Point(0, 0);
+            this.ribbon.MaxItemId = 1;
+            this.ribbon.Name = "ribbon";
+            this.ribbon.ShowApplicationButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbon.ShowCategoryInCaption = false;
+            this.ribbon.ShowExpandCollapseButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbon.ShowFullScreenButton = DevExpress.Utils.DefaultBoolean.False;
+            this.ribbon.ShowPageHeadersMode = DevExpress.XtraBars.Ribbon.ShowPageHeadersMode.Hide;
+            this.ribbon.ShowToolbarCustomizeItem = false;
+            this.ribbon.Size = new System.Drawing.Size(720, 32);
+            this.ribbon.StatusBar = this.ribbonStatusBar;
+            this.ribbon.Toolbar.ShowCustomizeItem = false;
+            // 
+            // ribbonStatusBar
+            // 
+            this.ribbonStatusBar.Location = new System.Drawing.Point(0, 369);
+            this.ribbonStatusBar.Name = "ribbonStatusBar";
+            this.ribbonStatusBar.Ribbon = this.ribbon;
+            this.ribbonStatusBar.Size = new System.Drawing.Size(720, 23);
+            // 
+            // gridControl
+            // 
+            this.gridControl.Cursor = System.Windows.Forms.Cursors.Default;
+            this.gridControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl.EmbeddedNavigator.AccessibleRole = System.Windows.Forms.AccessibleRole.ButtonDropDownGrid;
+            this.gridControl.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gridControl.Location = new System.Drawing.Point(0, 32);
+            this.gridControl.MainView = this.gridView;
+            this.gridControl.MenuManager = this.ribbon;
+            this.gridControl.MinimumSize = new System.Drawing.Size(400, 200);
+            this.gridControl.Name = "gridControl";
+            this.gridControl.Size = new System.Drawing.Size(720, 337);
+            this.gridControl.TabIndex = 9;
+            this.gridControl.UseEmbeddedNavigator = true;
+            this.gridControl.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView});
+            // 
+            // gridView
+            // 
+            this.gridView.Appearance.ColumnFilterButton.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.gridView.Appearance.ColumnFilterButton.Options.UseFont = true;
+            this.gridView.Appearance.FilterPanel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gridView.Appearance.FilterPanel.Options.UseFont = true;
+            this.gridView.Appearance.FocusedCell.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gridView.Appearance.FocusedCell.Options.UseFont = true;
+            this.gridView.Appearance.HeaderPanel.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.gridView.Appearance.HeaderPanel.Options.UseFont = true;
+            this.gridView.Appearance.Preview.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.gridView.Appearance.Preview.Options.UseFont = true;
+            this.gridView.Appearance.Row.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.gridView.Appearance.Row.Options.UseFont = true;
+            this.gridView.Appearance.RowSeparator.Font = new System.Drawing.Font("Tahoma", 9.25F);
+            this.gridView.Appearance.RowSeparator.Options.UseFont = true;
+            this.gridView.Appearance.TopNewRow.Font = new System.Drawing.Font("Tahoma", 9F);
+            this.gridView.Appearance.TopNewRow.Options.UseFont = true;
+            this.gridView.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.gridView.FocusRectStyle = DevExpress.XtraGrid.Views.Grid.DrawFocusRectStyle.None;
+            this.gridView.GridControl = this.gridControl;
+            this.gridView.IndicatorWidth = 30;
+            this.gridView.Name = "gridView";
+            this.gridView.OptionsBehavior.AllowDeleteRows = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView.OptionsBehavior.EditingMode = DevExpress.XtraGrid.Views.Grid.GridEditingMode.EditFormInplaceHideCurrentRow;
+            this.gridView.OptionsDetail.AllowOnlyOneMasterRowExpanded = true;
+            this.gridView.OptionsFind.AlwaysVisible = true;
+            this.gridView.OptionsFind.FindDelay = 400;
+            this.gridView.OptionsFind.SearchInPreview = true;
+            this.gridView.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView.OptionsSelection.InvertSelection = true;
+            this.gridView.OptionsSelection.MultiSelect = true;
+            this.gridView.OptionsView.ColumnAutoWidth = false;
+            this.gridView.OptionsView.ColumnHeaderAutoHeight = DevExpress.Utils.DefaultBoolean.True;
+            this.gridView.OptionsView.RowAutoHeight = true;
+            this.gridView.OptionsView.ShowGroupPanel = false;
+            // 
+            // InfoForm
+            // 
+            this.ActiveGlowColor = System.Drawing.Color.Blue;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 392);
+            this.Controls.Add(this.gridControl);
+            this.Controls.Add(this.ribbonStatusBar);
+            this.Controls.Add(this.ribbon);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Glow;
+            this.InactiveGlowColor = System.Drawing.Color.Red;
+            this.MaximizeBox = false;
+            this.Name = "InfoForm";
+            this.Ribbon = this.ribbon;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.StatusBar = this.ribbonStatusBar;
+            this.Text = "Информация о...";
+            this.Load += new System.EventHandler(this.InfoForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ribbon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl ribbon;
+        private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar;
+        private DevExpress.XtraGrid.GridControl gridControl;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView;
+    }
+}
---
+++ Elementors-arm/Base/InfoForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Data.OleDb;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace Elementors_arm
+{
+    public partial class InfoForm : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public InfoForm()
+        {
+            InitializeComponent();
+        }
+
+        private string connectionString; /* Переменная для строки подключения */
+
+        private void InfoForm_Load(object sender, EventArgs e)
+        {
+            DBOperator dbOperator = DBOperator.Instance;
+            gridControl.DataSource = dbOperator.Information();
+            gridView.BestFitColumns(); /* Оптимизированная ширина колонок */
+        }
+
+    }
+}
