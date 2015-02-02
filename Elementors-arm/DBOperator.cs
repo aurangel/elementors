bd58655bdef4992b4cd679babd89fe875f6ce6e2
Exoticness <roof1rst@gmail.com>
2015-02-02 23:06:11 +0300

    Added Main!

---
+++ Elementors-arm/DBOperator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Data.SQLite;
+using System.Data;
+
+namespace Elementors_arm {
+    public class DBOperator {
+        private static DBOperator instance = new DBOperator();
+
+        static DBOperator() { }
+
+        public static DBOperator Instance {
+            get { return instance; }
+        }
+       
+        public DataTable GetTableByName(string tableName) {
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=data.db;Version=3;");
+            m_dbConnection.Open();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("SELECT * FROM " + tableName, m_dbConnection);
+
+            DataTable dt = new DataTable(tableName);
+            adapter.Fill(dt);
+            
+            return dt;
+        }
+
+        public DataTable Information()
+        {
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=data.db;Version=3;");
+            m_dbConnection.Open();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("select Дисциплины.НаименованиеДисциплины, Образование.Образование, " +
+            "Образование.Аттестация, Образование.КвалификационнаяКатегория, Sum(Рейтинг.КоличествоБаллов) " +
+            "as КоличествоБаллов from ((Дисциплины inner join Преподаватели on Дисциплины.КодДисциплины " +
+            "= Преподаватели.КодДисциплины) inner join Образование on Преподаватели.КодПреподавателя " +
+            "= Образование.КодПреподавателя) inner join Рейтинг on Преподаватели.КодПреподавателя = " +
+            "Рейтинг.КодПреподавателя group by Дисциплины.НаименованиеДисциплины, Образование.Образование, " +
+            "Образование.Аттестация, Образование.КвалификационнаяКатегория, Преподаватели.КодПреподавателя " +
+            "having ((Преподаватели.КодПреподавателя) = " + Data.ValueOfPrepod + ")", m_dbConnection);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+        }
+
+        public DataTable Search()
+        {
+            SQLiteConnection m_dbConnection = new SQLiteConnection("Data Source=data.db;Version=3;");
+            m_dbConnection.Open();
+
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter("select Преподаватели.ФИО as ФИО, Рейтинг.КоличествоБаллов as КоличествоБаллов, " +
+            "Справочник_рейтинга.Наименование as Наименование from Преподаватели inner join " +
+            "(Справочник_рейтинга inner join Рейтинг on " +
+            "Справочник_рейтинга.КодРейтинга = Рейтинг.КодРейтинга) on " +
+            "Преподаватели.КодПреподавателя = Рейтинг.КодПреподавателя where " +
+            "((Преподаватели.ФИО)='" + Data.ValueOfSearch + "')", m_dbConnection);
+
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            return dt;
+        }
+            
+    }
+}
---
+++ Elementors-arm/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+
+namespace Elementors_arm
+{
+    static class Data
+    {
+        public static string ValueOfDoc { get; set; }
+        public static int ValueOfPrepod { get; set; }
+        public static string ValueOfReport { get; set; }
+        public static string ValueOfSearch { get; set; }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            DevExpress.UserSkins.BonusSkins.Register();
+            DevExpress.Skins.SkinManager.EnableFormSkins();
+            DevExpress.Skins.SkinManager.EnableMdiFormSkins();
+            DevExpress.LookAndFeel.UserLookAndFeel.Default.SetSkinStyle("Office 2013");
+            Application.Run(new HeaderForm());
+        }
+    }
+}
